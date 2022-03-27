76f897195bd778c0db7987bc28680b362454e338
yoram <arasy.yoram@yahoo.co.id>
2022-03-27 23:32:07 +0700

    Homework week 2-day 5: loop data track

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import "./App.css";
 import SearchComponent from "./components/search";
 import TrackComponent from "./components/track";
-import data from "./data";
+// import data from "./data";
+import data from "./datatrack";
 
 const SPOTIFY_CLIENT = process.env.REACT_APP_SPOTIFY_CLIENT;
-// console.log(SPOTIFY_CLIENT);
+console.log(SPOTIFY_CLIENT);
 
-const { name: songName, artists, album } = data;
+// const { name: songName, artists, album } = data;
+
+const renderTrack = data.map((e) => {
+  return (
+    <TrackComponent
+      key={e.id}
+      songName={e.name}
+      artists={e.artists}
+      album={e.album}
+    />
+  );
+});
 
 function App() {
   return (
     <section>
       <h1 className="playlist-title">Search Playlist</h1>
       <SearchComponent />
-      <TrackComponent songName={songName} artists={artists} album={album} />
+      <div className="card-container">{renderTrack}</div>
+      {/* <TrackComponent songName={songName} artists={artists} album={album} /> */}
     </section>
   );
 }
