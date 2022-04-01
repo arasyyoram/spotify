c35e59143275a201cd6cbd5b5017e92b3fcd2fd8
yoram <arasy.yoram@yahoo.co.id>
2022-04-01 13:02:06 +0700

    Homework week 3-day 3: add select and deselect feature

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,9 +1,12 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import TrackComponent from "../track";
-export default class SearchComponent extends Component {
-  state = { accessToken: "", query: "", tracks: [] };
+export default function SearchComponent() {
+  // state = { accessToken: "", query: "", tracks: [] };
+  const [accessToken, setAccessToken] = useState("");
+  const [query, setQuery] = useState("");
+  const [tracks, setTracks] = useState([]);
 
-  getQueryParams = () => {
+  const getQueryParams = () => {
     const hash = window.location.hash.substring(1);
     const params = {};
     hash.split("&").forEach((hk) => {
@@ -11,80 +14,137 @@ export default class SearchComponent extends Component {
       params[temp[0]] = temp[1];
     });
     // console.log(params.access_token);
-    this.setState({ accessToken: params.access_token });
+    setAccessToken(params.access_token);
     return params.access_token;
   };
 
-  componentDidMount() {
-    if (window.location.hash) {
-      this.getQueryParams();
-    }
-  }
+  // componentDidMount() {
+  //   if (window.location.hash) {
+  //     this.getQueryParams();
+  //   }
+  // }
 
-  getTracks = async (event) => {
+  useEffect(() => {
+    if (window.location.hash) {
+      getQueryParams();
+    }
+  }, []);
+
+  // const rollTracks = (event) => {
+  //   event.preventDefault();
+  //   setTracks((oldTrack) => {
+  //     if (oldTrack.length !== 0) {
+  //       oldTrack.map((track) => {
+  //         return track.isHeld ? track : getTracks();
+  //       });
+  //     }
+  //   });
+  // };
+
+  const getTracks = async (event) => {
     event.preventDefault();
     console.log("fetching...");
 
-    const tracks = await fetch(
-      `https://api.spotify.com/v1/search?q=${this.state.query}&type=artist%2Calbum%2Ctrack&market=ID&limit=8`,
+    const trackslist = await fetch(
+      `https://api.spotify.com/v1/search?q=${query}&type=artist%2Calbum%2Ctrack&market=ID&limit=8`,
       {
         method: "GET",
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          Authorization: "Bearer " + this.state.accessToken,
+          Authorization: "Bearer " + accessToken,
         },
       }
     ).then((response) => response.json());
-    this.setState({ tracks: tracks.albums.items });
+    const tempTrack = [];
+    trackslist.albums.items.forEach((e) => {
+      const tempObj = { ...e, isHeld: false };
+      tempTrack.push(tempObj);
+    });
+    // console.log(tracks.length);
+    if (tracks.length === 0) {
+      setTracks(tempTrack);
+    }
+    // else {
+    //   setTracks((oldTrack) =>
+    //     oldTrack.map((track) => {
+    //       return track.isHeld ? track : tempTrack;
+    //     })
+    //   );
+    // }
     // console.log(tracks);
+    if (tracks.length !== 0) {
+      // return setTracks(tempTrack);
+      setTracks((oldTrack) => {
+        let newTrack = [];
+        const selectedTrack = oldTrack.filter((track) => {
+          // console.log(track);
+          return track.isHeld;
+        });
+        // console.log(selectedTrack);
+        newTrack = [...selectedTrack, ...tempTrack];
+        return newTrack;
+      });
+    }
+
+    // setTracks(tracks.albums.items);
+    // console.log(tempTrack[0].isHeld);
   };
 
-  queryInput = (event) => {
-    this.setState({ query: event.target.value });
+  const queryInput = (event) => {
+    setQuery(event.target.value);
   };
 
-  render() {
-    const { tracks } = this.state;
+  // const { tracks } = this.state;
 
-    return (
-      <>
-        <div className="playlist-container">
-          <form id="playlist-form" className="form-container">
-            <div className="search-container">
-              <input
-                type="search"
-                id="search"
-                className="search-bar"
-                placeholder="Search your favorite song"
-                onChange={this.queryInput}
-              />
-            </div>
+  const getSelect = (id) => {
+    console.log(id);
+    setTracks((oldTrack) =>
+      oldTrack.map((track) => {
+        return track.uri === id ? { ...track, isHeld: !track.isHeld } : track;
+      })
+    );
+  };
 
-            <button id="playlist-btn" onClick={this.getTracks}>
-              Search
-            </button>
-          </form>
-          {/* <img
+  return (
+    <>
+      <div className="playlist-container">
+        <form id="playlist-form" className="form-container">
+          <div className="search-container">
+            <input
+              type="search"
+              id="search"
+              className="search-bar"
+              placeholder="Search your favorite song"
+              onChange={queryInput}
+            />
+          </div>
+
+          <button id="playlist-btn" onClick={getTracks}>
+            Search
+          </button>
+        </form>
+        {/* <img
         src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"
         alt="images"
         className="img"
       /> */}
-        </div>
-        <div className="card-container">
-          {tracks.length > 0 &&
-            tracks.map((e) => {
-              return (
-                <TrackComponent
-                  key={e.id}
-                  songName={e.name}
-                  artists={e.artists}
-                  album={e.images}
-                />
-              );
-            })}
-        </div>
-      </>
-    );
-  }
+      </div>
+      <div className="card-container">
+        {tracks.length > 0 &&
+          tracks.map((e) => {
+            return (
+              <TrackComponent
+                key={e.id}
+                songName={e.name}
+                artists={e.artists}
+                album={e.images}
+                isHeld={e.isHeld}
+                getSelect={() => getSelect(e.uri)}
+              />
+            );
+          })}
+      </div>
+    </>
+  );
 }
--- src/components/track/index.js
+++ src/components/track/index.js
@@ -1,4 +1,15 @@
-export default function TrackComponent({ songName, artists, album }) {
+export default function TrackComponent({
+  songName,
+  artists,
+  album,
+  isHeld,
+  getSelect,
+}) {
+  const style = {
+    backgroundColor: isHeld ? "#000" : "#fff",
+    color: isHeld ? "#fff" : "#000",
+  };
+
   return (
     <div className="card">
       <div className="card-img">
@@ -6,7 +17,9 @@ export default function TrackComponent({ songName, artists, album }) {
       </div>
       <h3 className="card-title">{songName}</h3>
       <p className="card-artist">{artists[0].name}</p>
-      <button className="card-btn">Select</button>
+      <button className="card-btn" onClick={getSelect} style={style}>
+        {isHeld ? "Deselect" : "Select"}
+      </button>
     </div>
   );
 }
